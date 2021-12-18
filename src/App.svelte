ea985f715ea466d35e7525bac75906679b4dcbc3
Sereme <seremeabd@gmail.com>
2021-12-18 18:05:07 +0000

    Mytodo

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+  let tasks = [
+    {
+      name: "Task",
+      complete: false,
+    },
+  ];
+
+  let newTasks = "";
+
+  let addTask = () => {
+    tasks = [
+      ...tasks,
+      {
+        id: Date.now(),
+        name: newTasks,
+        complete: false,
+      },
+    ];
+    newTasks = "";
+  };
+
+  let deleteTask = (id) => {
+    tasks = [...tasks.filter((t) => t.id != id)];
+  };
+</script>
+
+<main>
+  <h3>MyTodo </h3>
+  <ul>
+    {#each tasks as task}
+      <li>
+        <input type="checkbox" bind:checked={task.complete} />
+        <span class:checked={task.complete}>{task.name}</span>
+        <button on:click={deleteTask(task.id)}>X</button>
+      </li>
+    {/each}
+  </ul>
+  <input type="text" name="" id="" bind:value={newTasks} />
+  <button on:click|preventDefault={addTask}> add task</button>
+  {JSON.stringify(tasks)}
+</main>
+
+<style>
+  .checked {
+    text-decoration: line-through;
+  }
+</style>
